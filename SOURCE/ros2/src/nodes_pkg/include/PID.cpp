0dfff07e586ab3183a94003251500cddc766dad0
BenSwisa <benebyrulls@gmail.com>
2022-09-08 15:30:30 +0300

    tension val node

---
+++ SOURCE/ros2/src/nodes_pkg/include/PID.cpp
@@ -0,0 +1,87 @@
+/**
+ */
+
+#ifndef _PID_SOURCE_
+#define _PID_SOURCE_
+
+#include <iostream>
+#include <cmath>
+#include "PID.h"
+
+using namespace std;
+
+//PIDImpl::PIDImpl( double dt, double max, double min, double Kp, double Ki, double Kd ) :
+PID::PID( double dt, double max, double min, double Kp, double Ki, double Kd ) :
+        _dt(dt),
+        _max(max),
+        _min(min),
+        _Kp(Kp),
+        _Kd(Kd),
+        _Ki(Ki),
+        _pre_error(0),
+        _integral(0)
+{
+}
+
+PID::~PID()
+{
+}
+
+//double PIDImpl::calculate( double setpoint, double pv )
+double PID::calculate( double setpoint, double pv )
+{
+    // Calculate error
+    double error = setpoint - pv;
+    // Proportional term
+    this->Pout = _Kp * error;
+    // Integral term
+    if (_pre_error / error < 0)
+        _integral = 0;          // Clamping
+    else
+        _integral += error * _dt;
+    this->Iout = _Ki * _integral;
+
+    // Derivative term
+    double derivative = (error - _pre_error) / _dt;
+    double Dout = _Kd * derivative;
+
+    // Calculate total output
+    double output = Pout + Iout + Dout;
+
+    // Restrict to max/min
+    if( output > _max )
+        output = _max;
+    else if( output < _min )
+        output = _min;
+
+    // Save error to previous error
+    this->_pre_error = error;
+
+    // Debug
+    //std::cout << "calc: Iout" << Iout << endl;
+//    std::cout << "error: " << error << "\tPout: " << Pout << "\tDout: " << Dout << "\tIout: " << Iout << "\toutput: " << output << endl;
+
+
+    return output;
+}
+
+void PID::resetSum(){
+    this->_integral = 0;
+}
+double PID :: getsum(){
+    return _integral;
+}
+void PID::setK( double dt, double max, double min, double Kp, double Ki, double Kd  ){
+    this->_dt = dt;
+    this->_max = max;
+    this->_min = min;
+
+    this->_Kp = Kp;
+    this->_Kd = Kd;
+    this->_Ki = Ki;
+
+    // Debug
+    std::cout << "setK: Kp" << this->_Kp << endl;
+}
+
+#endif
---
+++ SOURCE/ros2/src/nodes_pkg/include/PID.h
@@ -0,0 +1,40 @@
+/**
+ */
+
+#ifndef _PID_H_
+#define _PID_H_
+
+class PID
+{
+public:
+    // Kp -  proportional gain
+    // Ki -  Integral gain
+    // Kd -  derivative gain
+    // dt -  loop interval time
+    // max - maximum value of manipulated variable
+    // min - minimum value of manipulated variable
+    //PID();
+    PID( double dt, double max, double min, double Kp, double Ki, double Kd );
+
+    // Returns the manipulated variable given a setpoint and current process value
+    double calculate( double setpoint, double pv );
+    void resetSum();
+    double getsum ();
+    void setK(double dt, double max, double min, double Kp, double Ki, double Kd);
+    ~PID();
+    double Iout;
+    double Pout;
+
+private:
+    //PIDImpl *pimpl;
+    double _dt;
+    double _max;
+    double _min;
+    double _Kp;
+    double _Kd;
+    double _Ki;
+    double _pre_error;
+    double _integral;
+};
+
+#endif
