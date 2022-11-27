23c5d92002ff2472b08cddcc4a7fb27c51a665ad
BenSwisa <benebyrulls@gmail.com>
2022-11-27 13:15:16 +0200

    update for motor switch 7->1

--- SOURCE/ros2/src/nodes_pkg/src/node1.cpp
+++ SOURCE/ros2/src/nodes_pkg/src/node1.cpp
@@ -15,12 +15,16 @@ public:
     Node1() : Node("node1") 
     {
      RCLCPP_INFO(this->get_logger(), "Node1 has been started.");
+     // subscriber to encoder current values
      joint_val_subscriber_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
       "joint_val_topic", 10, std::bind(&Node1::joint_val_callback, this, _1));
+      // sbuscriber to tension values
     tension_val_subscriber_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
       "tension_val_topic", 10, std::bind(&Node1::tension_val_callback, this, _1));
+      // subscriber to joint angles requested
     joint_cmd_subscriber_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
       "joint_cmd_topic", 10, std::bind(&Node1::joint_cmd_callback, this, _1));
+      // pwm command publisher to motors
      motor_cmd_publisher_ = this->create_publisher<std_msgs::msg::Int32MultiArray>("motor_cmd_topic",10);
     //  timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000)),
     //                                      std::bind(&Node1::publish_motor_pwm ,this)); //publish motor cmd every second  
@@ -29,28 +33,28 @@ public:
 
  
 private:
-    void publish_motor_pwm(int *arr){
-        auto message = std_msgs::msg::Int32MultiArray();
-        message.data={0,0,0,0,0,0,0,0,0,0,0,0};
-        for(int i=0;i<12;i++){
-        message.data[i]=arr[i];
-        }
-        motor_cmd_publisher_->publish(message);
+    // void publish_motor_pwm(int *arr){
+    //     auto message = std_msgs::msg::Int32MultiArray();
+    //     message.data={0,0,0,0,0,0,0,0,0,0,0,0};
+    //     for(int i=0;i<12;i++){
+    //     message.data[i]=arr[i];
+    //     }
+    //     motor_cmd_publisher_->publish(message);
 
-    }
+    // }
 
     void joint_val_callback(const std_msgs::msg::Float32MultiArray msg){
       RCLCPP_INFO(this->get_logger(), " %f , %f \n",msg.data[0],msg.data[1]);
     }
 
-    void joint_cmd_callback(const std_msgs::msg::Float32MultiArray msg){
-      RCLCPP_INFO(this->get_logger(), " %f , %f \n",msg.data[0],msg.data[1]);
-      auto message = std_msgs::msg::Int32MultiArray();
-        message.data={0,0,0,0,0,0,0,0,0,0,0,0};
-        for(int i=0;i<12;i++){
-        message.data[i]=(int32_t)msg.data[i];
-        }
-        motor_cmd_publisher_->publish(message);
+    void joint_cmd_callback(const std_msgs::msg::Float32MultiArray msg){ 
+      // RCLCPP_INFO(this->get_logger(), " %f , %f \n",msg.data[0],msg.data[1]);
+      // auto message = std_msgs::msg::Int32MultiArray();
+      //   message.data={0,0,0,0,0,0,0,0,0,0,0,0};
+      //   for(int i=0;i<12;i++){
+      //   message.data[i]=(int32_t)msg.data[i];
+      //   }
+      //   motor_cmd_publisher_->publish(message);
     }
     
     void tension_val_callback(const std_msgs::msg::Float32MultiArray msg){
